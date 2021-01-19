b25b99bde09caf3fa620b271de3c50a06dd5e76c
Abdulaziz Alnaim <45672672+211513441@users.noreply.github.com>
2021-01-19 06:40:47 +0300

    Add files via upload

---
+++ exp4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main() {
+	int a, b;
+	a = b = 5;
+
+	printf("postfix mode and prefix mode example\n");
+	printf("initial value a = b = 5\n");
+	printf("\npostfix mode, a-- = %d prefix mode, --b = %d", a--, --b);
+	printf("\npostfix mode, a-- = %d prefix mode, --b = %d", a--, --b);
+	printf("\npostfix mode, a-- = %d prefix mode, --b = %d", a--, --b);
+	printf("\npostfix mode, a-- = %d prefix mode, --b = %d", a--, --b);
+	printf("\npostfix mode, a-- = %d prefix mode, --b = %d", a--, --b);
+	
+}
